92a78a13315d8f988a3b168aaa0ca3f67fa3a8ca
Kyrylo Stepanov <kyrylo.stepanov@gmail.com>
2021-11-04 22:50:14 +0200

    basic transactions

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -9,18 +9,18 @@ impl Blockchain {
         for (i, block) in self.blocks.iter().enumerate() {
             if i as u32 != block.index {
                 println!("Block index mismatch {} != {}", i, block.index);
-                return false
-            } else if !block::check_difficulty(&block.hash(), block.difficulty)  {
+                return false;
+            } else if !block::check_difficulty(&block.hash(), block.difficulty) {
                 println!("Block {} difficulty mismatch", i);
-                return false
+                return false;
             } else if i != 0 {
                 let prev_block = &self.blocks[i - 1];
                 if block.timestamp <= prev_block.timestamp {
                     println!("Time should increase");
-                    return false
+                    return false;
                 } else if block.previous_block_hash != prev_block.hash {
                     print!("Block linked hashes mismatch");
-                    return false
+                    return false;
                 }
             } else {
                 if block.previous_block_hash != [0; 32] {
@@ -28,6 +28,6 @@ impl Blockchain {
                 }
             }
         }
-        return true
+        return true;
     }
 }
